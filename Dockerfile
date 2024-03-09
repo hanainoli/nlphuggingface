85d524f21f93b8a06f3f10fa697c1f5f0eb555b7
hanainoli <nagaraju.cheelam@gmail.com>
2024-03-09 17:34:10 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use the official Python image as the base image
+FROM python:3.9-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the requirements file and install dependencies
+COPY requirements.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the FastAPI application files into the container
+COPY app.py .
+
+# Expose the port on which your FastAPI application will run
+EXPOSE 8000
+
+# Command to run the FastAPI application using Uvicorn
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+from transformers import pipeline
+
+# Specify the model by its name
+classifier = pipeline("sentiment-analysis")
+app = FastAPI()
+
+data = ["I love u", "I hate you"]
+
+classifier(data)
+
+class RequestModel(BaseModel):
+    input_string: str
+@app.post("/analyse")
+def your_function(request: RequestModel):
+        input_string = request.input_string
+        sentiment = classifier(input_string)
+        return {"result":
+            { "sentiment" : sentiment[0]["label"],
+            "score" : sentiment[0]["score"]
+            }
+        }
